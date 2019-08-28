01a00dbe0d0b5f64994e6d9bf8a895605db65baa
TrungAnhDo <101866520@student.swin.edu.au>
2019-08-28 23:47:16 +1000

    Add mouse hover, mouse click and show info on Marker (basic)

--- src/Map.js
+++ src/Map.js
@@ -3,20 +3,56 @@ import {
   withGoogleMap,
   GoogleMap,
   Marker,
+  InfoWindow,
   withScriptjs
 } from "react-google-maps";
+
+var isShowInfoWindow = false;
 
 const Map = withScriptjs(
   withGoogleMap(props => (
     <GoogleMap defaultZoom={12} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-      <Marker position={{ lat: -34.397, lng: 150.644 }} />
+      {
+        props.isMarkerShown &&
+        <Marker position={{ lat: -34.397, lng: 150.644 }} 
+          onClick={(e)=>onMarkerClick(e)}
+          onMouseOver={(e)=>onMarkerHoverOn(e)}
+          onMouseOut={(e)=>onMarkerHoverOff(e)}
+        >
+          <InfoWindow 
+            //onClose = {}
+            visible={isShowInfoWindow}>
+            <div>
+              <p>{'Showing something in the info window'}</p>
+            </div>
+          </InfoWindow>
+        </Marker>
+      }
     </GoogleMap>
   ))
 );
 
+function onMarkerClick(e) 
+{
+  return console.log('Clicked at' + e.latLng.lat());
+}
+
+function onMarkerHoverOn(e) {
+  //return console.log(marker);
+  isShowInfoWindow = true;
+  return console.log('Show Info Window: ' + isShowInfoWindow);
+}
+
+function onMarkerHoverOff(e) {
+  //return console.clear();
+  isShowInfoWindow = false;
+  return console.log('Show Info Window: ' + isShowInfoWindow);
+}
+
 export default props => (
   <Map
-    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCo3IgVCx38MJbR-x-O-QYJZgnZK-HhKqU"
+    isMarkerShown = {true}
+    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDQNVlrpDGGHrW5wdTK2tPbB0S8SwWlL7w"
     loadingElement={<div />}
     containerElement={<div style={{ height: "100%", width: "100%" }} />}
     mapElement={<div style={{ height: "100%", width: "100%" }} />}
