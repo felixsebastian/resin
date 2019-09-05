90317aa106049fc21454c9f7fb10bab5c2601f61
TrungAnhDo <101866520@student.swin.edu.au>
2019-09-05 20:20:16 +1000

    fix Geo Marker and functionalities during merge.

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Header from "./Header.js";
+import Geo from "./Geo.js";
+import Time from "./Time.js";
+import { css } from "glamor";
+
+// styles
+
+const islandCss = css({
+  border: "0.5rem solid #eee"
+});
+
+const appCss = css({
+  display: "flex",
+  "flex-direction": "column",
+  height: "100vh",
+  width: "1200px",
+  "max-width": "100%",
+  margin: "0 auto"
+});
+
+const headerCss = css({
+  padding: "1rem 2rem",
+  "border-bottom": 0
+});
+
+const viewportRowCss = css({
+  display: "flex"
+});
+
+const geoCss = css({
+  "border-bottom": 0
+});
+
+const timeCss = css({});
+
+const viewportsFrameCss = css({
+  display: "flex",
+  "flex-direction": "column"
+});
+
+// component
+
+export default function App() {
+  return (
+    <div className={appCss}>
+      <Header classNameCss={`${headerCss} ${islandCss}`} />
+      <div className={`${viewportsFrameCss}`} style={{ flexGrow: 1 }}>
+    {   <Geo
+          classNameCss={`${geoCss} ${islandCss} ${viewportRowCss}`}
+          style={{ flexGrow: 1 }}
+        />}
+        <Time
+          classNameCss={`${timeCss} ${islandCss} ${viewportRowCss}`}
+          style={{ flexGrow: 1 }}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/Map.js
@@ -0,0 +1,82 @@
+import React from "react";
+import {
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow,
+  withScriptjs,
+  OverlayView,
+} from "react-google-maps";
+import DataPanel from "./DataPanel";
+import { css } from "glamor";
+import { compose, withStateHandlers, withProps } from "recompose";
+import { geo } from "./data";
+
+const dataPanelCss = css({});
+
+const Map =
+  compose(
+    withProps({
+      googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDQNVlrpDGGHrW5wdTK2tPbB0S8SwWlL7w",
+      loadingElement: <div />,
+      containerElement: <div style={{ height: "100%", width: "100%" }} />,
+      mapElement: <div style={{ height: "100%", width: "100%" }} />,
+    }),
+    withStateHandlers(() => ({
+      isOpen: false, isClicked: false,
+    }), {
+      onToggleOpen: ({ isOpen }) => () => ({
+        isOpen: !isOpen,
+      }),
+      onClicked: ({ isClicked }) => () => ({
+        isClicked: !isClicked,
+      })
+    }), 
+  withScriptjs,
+  withGoogleMap)(props => (
+    <GoogleMap defaultZoom={12} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+      {
+        props.isMarkerShown &&
+        <Marker 
+          npm position={{ lat: -34.397, lng: 150.644 }} 
+          onClick={props.onClicked}
+          onMouseOver={props.onToggleOpen}
+          onMouseOut={props.onToggleOpen}
+        >
+          {
+            props.isOpen &&
+            <InfoWindow onCloseClick={props.onToggleOpen}>
+              <div>
+                <p>{'Showing something in the info window'}</p>
+              </div>
+            </InfoWindow>
+          } 
+          { props.isClicked &&
+            <OverlayView
+              position={{ lat: -34.397, lng: 150.644 }}
+              mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+            >
+              <div style={{ background: `white`, border: `1px solid #ccc`, padding: 15}}>
+                <h1>Overlay View</h1>
+                <button 
+                  onClick={props.onClicked} 
+                  style={{ left: '91%', top: '10%', position: 'absolute'}}>
+                  X
+                </button>
+                <DataPanel
+                  className={dataPanelCss}
+                  items={geo}
+                />
+              </div>
+            </OverlayView>
+          }
+        </Marker>
+      }
+    </GoogleMap>
+  ));
+
+export default props => (
+  <Map
+    isMarkerShown = {true}
+  />
+);
