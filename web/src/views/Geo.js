3ae4dae75c9824fcb65bbcc8eaee678371faec38
101866520 <101866520@student.swin.edu.au>
2019-09-22 10:27:43 +1000

    resove conflicts

--- web/src/views/Geo.js
+++ web/src/views/Geo.js
@@ -13,16 +13,7 @@ const mapCss = css({ overflow: "hidden", position: "static" });
 export default function Geo({ classNameCss, style }) {
   return (
     <div className={classNameCss} style={style}>
-<<<<<<< HEAD:web/src/views/Geo.js
       <DataPanel className={dataPanelCss} items={geo} />
-=======
-      {/*
-      <DataPanel
-        className={dataPanelCss}
-        items={geo}
-      />
-      */}
->>>>>>> Attempted to implement multiple markers and marker generation:src/Geo.js
       <Map classNameCss={mapCss} style={{ flexGrow: 1 }} />
     </div>
   );
