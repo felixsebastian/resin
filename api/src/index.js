2fa1e128b2f8ebf400a49a4e8c444941dc81d9aa
Luke Mcwha <luke.mcwha@gmail.com>
2019-09-30 12:09:29 +1000

    Added running details to index

--- api/src/index.js
+++ api/src/index.js
@@ -18,3 +18,28 @@ server.applyMiddleware({ app });
 app.listen({ port: 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 );
+
+
+
+// Dev commands to know
+/*
+  Run backend:                  docker-compose up rdb api
+  Run backend in background:    docker-compose up -d rdb api
+  
+  Sequence of commands first run: 
+        docker-compose up
+    New Tab:
+        docker-compose exec api bash
+        npm run-script setup
+        exit
+
+  Migrate db changes to db:
+    docker-compose exec api bash
+    npm run-script migrate
+    exit
+
+  Find full list of scripts in api/package.json
+
+  For information on Sequelize db Fucntions check out:
+  http://zetcode.com/javascript/sequelize/
+*/
