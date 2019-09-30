884df529195fa2fa379fdf981efb9e14805a0679
Felix Sebastian <hello@felix-sebastian.com>
2019-09-30 18:41:57 +1000

    fix

--- web/src/components/EventDetails.js
+++ web/src/components/EventDetails.js
@@ -8,7 +8,7 @@ import {
 } from "carbon-components-react";
 import styled from "styled-components";
 import Viewport from "./Viewport";
-import { clickToggleInfoPanel } from "./action";
+import { selectIncident } from "../lib/actionCreators";
 import { connect } from "react-redux";
 
 const data = {
@@ -23,36 +23,41 @@ const Layout = styled(Viewport)`
 
 const mapStateToProps = state => {
   return {
-      infoPanelIsOpen: state.infoPanelIsOpen
-  }
-}
+    infoPanelIsOpen: state.infoPanelIsOpen
+  };
+};
 
 function mapDispatchToProps(dispatch) {
   return {
-      clickTogglePanel: () => dispatch(clickToggleInfoPanel())
-  }
+    clickTogglePanel: () => dispatch(selectIncident())
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(({infoPanelIsOpen}) => (
-  infoPanelIsOpen == true &&
-  <Layout>
-    <StructuredListWrapper>
-      <StructuredListHead>
-        <StructuredListCell head style={{ width: "33%" }}>
-          Key
-        </StructuredListCell>
-        <StructuredListCell head>Value</StructuredListCell>
-      </StructuredListHead>
-      <StructuredListBody>
-        {Object.keys(data).map(key => (
-          <StructuredListRow>
-            <StructuredListCell>{key}:</StructuredListCell>
-            <StructuredListCell>
-              <b>{data[key]}</b>
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(
+  ({ infoPanelIsOpen }) =>
+    infoPanelIsOpen == true && (
+      <Layout>
+        <StructuredListWrapper>
+          <StructuredListHead>
+            <StructuredListCell head style={{ width: "33%" }}>
+              Key
             </StructuredListCell>
-          </StructuredListRow>
-        ))}
-      </StructuredListBody>
-    </StructuredListWrapper>
-  </Layout> 
-));
+            <StructuredListCell head>Value</StructuredListCell>
+          </StructuredListHead>
+          <StructuredListBody>
+            {Object.keys(data).map(key => (
+              <StructuredListRow>
+                <StructuredListCell>{key}:</StructuredListCell>
+                <StructuredListCell>
+                  <b>{data[key]}</b>
+                </StructuredListCell>
+              </StructuredListRow>
+            ))}
+          </StructuredListBody>
+        </StructuredListWrapper>
+      </Layout>
+    )
+);
