de9deb1aab01f4b4c6e26a1894bd9b4190923cb9
Felix Sebastian <hello@felix-sebastian.com>
2019-10-03 15:34:45 +1000

    event details panel show unconditionally

--- web/src/components/EventDetails.js
+++ web/src/components/EventDetails.js
@@ -8,7 +8,6 @@ import {
 } from "carbon-components-react";
 import styled from "styled-components";
 import Viewport from "./Viewport";
-import { connect } from "react-redux";
 
 const data = {
   make: "mitsubishi",
@@ -20,33 +19,25 @@ const Layout = styled(Viewport)`
   flex-grow: 1;
 `;
 
-export default connect(
-  state => ({
-    infoPanelIsOpen: !!state.selection.length
-  }),
-  null
-)(
-  ({ infoPanelIsOpen }) =>
-    infoPanelIsOpen === true && (
-      <Layout>
-        <StructuredListWrapper>
-          <StructuredListHead>
-            <StructuredListCell head style={{ width: "33%" }}>
-              Key
+export default ({ infoPanelIsOpen }) => (
+  <Layout>
+    <StructuredListWrapper>
+      <StructuredListHead>
+        <StructuredListCell head style={{ width: "33%" }}>
+          Key
+        </StructuredListCell>
+        <StructuredListCell head>Value</StructuredListCell>
+      </StructuredListHead>
+      <StructuredListBody>
+        {Object.keys(data).map(key => (
+          <StructuredListRow>
+            <StructuredListCell>{key}:</StructuredListCell>
+            <StructuredListCell>
+              <b>{data[key]}</b>
             </StructuredListCell>
-            <StructuredListCell head>Value</StructuredListCell>
-          </StructuredListHead>
-          <StructuredListBody>
-            {Object.keys(data).map(key => (
-              <StructuredListRow>
-                <StructuredListCell>{key}:</StructuredListCell>
-                <StructuredListCell>
-                  <b>{data[key]}</b>
-                </StructuredListCell>
-              </StructuredListRow>
-            ))}
-          </StructuredListBody>
-        </StructuredListWrapper>
-      </Layout>
-    )
+          </StructuredListRow>
+        ))}
+      </StructuredListBody>
+    </StructuredListWrapper>
+  </Layout>
 );
