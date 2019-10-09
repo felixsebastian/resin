4e4aad1a2abba8581a3bf10fcc0dd93c02f165d4
Felix Sebastian <hello@felix-sebastian.com>
2019-10-09 22:20:53 +1100

    remove junk from queries file

--- api/src/schema/queries.js
+++ api/src/schema/queries.js
@@ -1,13 +1,7 @@
-import types from "./types";
 export default `
   type Query {
     incidents: [Incident!]
     vehicles: [Vehicle!]
     sensors: [Sensor!]
   }
-  `;
-
-// type Query {
-//   getEvents(timerange: Timerange): [Event]
-//   getVehicle: Vehicle
-// }
+`;
