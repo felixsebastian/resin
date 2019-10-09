878a5f78cdbb6ffcf88311311514406381503c02
Felix Sebastian <hello@felix-sebastian.com>
2019-10-10 10:34:35 +1100

    show username on home page

--- web/src/components/Home.js
+++ web/src/components/Home.js
@@ -1,18 +1,21 @@
 import React from "react";
 import CentredLayout from "./CentredLayout";
+import connect from "../lib/connect";
 
-export default () => (
-  <CentredLayout>
-    <p>
-      <b>Welcome to ResIN</b>
-    </p>
-    <p>
-      Created by students of Swinburne University:{" "}
-      <a href="http://github.com/felix-sebastian/">Trung Ahn Do</a>,{" "}
-      <a href="http://github.com/felix-sebastian/">Luke McWha</a>,{" "}
-      <a href="http://github.com/felix-sebastian/">Cory Misfud</a>,{" "}
-      <a href="http://github.com/felix-sebastian/">Rab'a Albreiki</a> &amp;{" "}
-      <a href="http://github.com/felix-sebastian/">Felix Sebastian</a>
-    </p>
-  </CentredLayout>
+export default connect(state => ({ username: state.user.username }))(
+  ({ username }) => (
+    <CentredLayout>
+      <p>
+        <b>Welcome to ResIN{username && ", " + username}</b>
+      </p>
+      <p>
+        Created by students of Swinburne University:{" "}
+        <a href="http://github.com/felix-sebastian/">Trung Ahn Do</a>,{" "}
+        <a href="http://github.com/felix-sebastian/">Luke McWha</a>,{" "}
+        <a href="http://github.com/felix-sebastian/">Cory Misfud</a>,{" "}
+        <a href="http://github.com/felix-sebastian/">Rab'a Albreiki</a> &amp;{" "}
+        <a href="http://github.com/felix-sebastian/">Felix Sebastian</a>
+      </p>
+    </CentredLayout>
+  )
 );
