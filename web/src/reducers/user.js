e4c11b411afed7c8092f5fac27be2118fc96a828
Felix Sebastian <hello@felix-sebastian.com>
2019-10-10 10:34:10 +1100

    use cookie on page load

--- web/src/reducers/user.js
+++ web/src/reducers/user.js
@@ -1,10 +1,12 @@
 import types from "../lib/actionTypes";
+import cookie from "js-cookie";
 
-export default (state = null, action) => {
+export default (state = cookie.get("user") || null, action) => {
   switch (action.type) {
     case types.LOG_IN:
       return action.payload;
     case types.LOG_OUT:
+      cookie.remove("user");
       return null;
     default:
       return state;
