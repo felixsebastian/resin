5e08542706ea344aea321287c1a8cd6c7ca5ea9d
Felix Sebastian <hello@felix-sebastian.com>
2019-10-13 11:37:14 +1100

    fix incident marker

--- web/src/components/IncidentMarker.js
+++ web/src/components/IncidentMarker.js
@@ -1,94 +1,52 @@
-import React, { useState, useEffect } from "react";
-import { compose, withStateHandlers } from "recompose";
+import React, { useState } from "react";
 import { Marker, InfoWindow, Circle } from "react-google-maps";
-import store from "../lib/store";
-import { selectIncident, addIncidentToSelection } from "../actions";
+import connect from "../lib/connect";
 
-function useKey(key) {
-  // Keep track of key state
-  const [pressed, setPressed] = useState(false);
-
-  // Does an event match the key we're watching?
-  const match = event => key.toLowerCase() == event.key.toLowerCase();
-
-  // Event handlers
-  const onDown = event => {
-    if (match(event)) setPressed(true);
-  };
-
-  const onUp = event => {
-    if (match(event)) setPressed(false);
-  };
-
-  // Bind and unbind events
-  useEffect(() => {
-    window.addEventListener("keydown", onDown);
-    window.addEventListener("keyup", onUp);
-    return () => {
-      window.removeEventListener("keydown", onDown);
-      window.removeEventListener("keyup", onUp);
-    };
-  }, [key]);
-
-  return pressed;
-}
-
-const Incident = compose(
-  withStateHandlers(
-    () => ({
-      isOpen: false,
-      isClicked: false
-    }),
-    {
-      onToggleOpen: ({ isOpen }) => () => ({
-        isOpen: !isOpen
-      }),
-      onClickedOpen: ({ isClicked }) => marker => ({
-        isClicked: !isClicked
-      })
-    }
-  )(props => {
-    const isShiftPressed = useKey("shift");
-
-    return (
-      <Marker
-        position={props.marker}
-        onClick={() => {
-          if (isShiftPressed)
-            store.dispatch(addIncidentToSelection(props.marker.id));
-          else store.dispatch(selectIncident(props.marker.id));
-          props.onClickedOpen();
-        }}
-        onMouseOver={props.onToggleOpen}
-        onMouseOut={props.onToggleOpen}
-        icon={"./marker.png"}
-      >
-        {props.isOpen && (
-          <InfoWindow onCloseClick={props.onToggleOpen}>
-            <div>
-              <p>
-                {"{"}
-                {props.marker.lat}
-                {","}
-                {props.marker.lng}
-                {"}"}
-              </p>
-            </div>
-          </InfoWindow>
-        )}
-        {props.isClicked && (
-          <Circle
-            center={props.marker}
-            radius={100}
-            options={{
-              fillColor: "red",
-              strokeColor: "red"
-            }}
-          />
-        )}
-      </Marker>
-    );
-  })
+const Halo = props => (
+  <Circle
+    center={props.center}
+    radius={20}
+    options={{
+      fillColor: "red",
+      strokeColor: "red"
+    }}
+  />
 );
 
-export default Incident;
+const Info = props => (
+  <InfoWindow onCloseClick={props.onToggleOpen}>
+    <div>
+      <p>
+        {"{"}
+        {props.latitude}
+        {","}
+        {props.longitude}
+        {"}"}
+      </p>
+    </div>
+  </InfoWindow>
+);
+
+export default connect((state, props) => ({
+  isShiftKeyDown: state.keysDown.includes("Shift"),
+  selected: state.selection.includes(props.marker.id)
+}))(props => {
+  const { marker } = props;
+  const [state, setState] = useState({ isOpen: false });
+
+  return (
+    <Marker
+      position={marker}
+      onClick={() =>
+        props.isShiftKeyDown
+          ? props.actions.addIncidentToSelection(marker.id)
+          : props.actions.selectIncident(marker.id)
+      }
+      onMouseOver={() => setState({ ...state, isOpen: true })}
+      onMouseOut={() => setState({ ...state, isOpen: false })}
+    >
+      {props.isOpen && <Info latitude={marker.lat} longitude={marker.lng} />}
+      {props.selected && <Halo center={marker} />}
+    </Marker>
+  );
+});
