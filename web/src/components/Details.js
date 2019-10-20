a31f9aeb12d663db6d04c797b66d4e881063a949
Felix Sebastian <hello@felix-sebastian.com>
2019-10-20 11:17:11 +1100

    move to redux hooks

--- web/src/components/Details.js
+++ web/src/components/Details.js
@@ -6,9 +6,6 @@ import {
   StructuredListRow,
   StructuredListCell
 } from "carbon-components-react";
-import styled from "styled-components";
-import Viewport from "./Viewport";
-import connect from "../lib/connect";
 import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 import Button from "./Button";
@@ -16,30 +13,22 @@ import Hint from "./Hint";
 import Centered from "./layouts/Centered";
 import Padding from "./Padding";
 import sources from "../lib/incidentSources";
-
-const fields = {
-  timestamp: { canFilterBy: false, label: "Time" },
-  latitude: { canFilterBy: false, label: "Latitude" },
-  longitude: { canFilterBy: false, label: "Longitude" },
-  numVehicles: { canFilterBy: true, label: "Vehicle count" },
-  damageSeverity: { canFilterBy: true, label: "Damage" },
-  description: { canFilterBy: true, label: "Notes" },
-  dca: { canFilterBy: true, label: "DCA" }
-};
+import { useSelector } from "react-redux";
 
 const INCIDENTS = gql`
   {
     incidents {
       id
       timestamp
-      latitude
-      longitude
+      location {
+        latitude
+        longitude
+      }
       numVehicles
       damageSeverity
       description
       dca
-      weatherDesc
-      vehicle1 {
+      vehicles {
         vin
         type
         registration
@@ -56,86 +45,76 @@ const INCIDENTS = gql`
   }
 `;
 
-const Box = styled(Viewport)`
-  flex-grow: 1;
-`;
-
-export default connect(state => ({
-  isSelectionPresent: !!state.selection.length,
-  filteringFields: state.filteringFields,
-  selection: state.selection
-}))(({ isSelectionPresent, actions, selection, filteringFields }) => {
+export default () => {
+  const selection = useSelector(state => state.selection);
+  const isSelectionPresent = useSelector(state => !!state.selection.length);
   const { loading, error, data } = useQuery(INCIDENTS);
-  if (loading) return <p>loading...</p>;
-  if (error) return <p>error!</p>;
+  if (loading) return <Padding>loading...</Padding>;
+  if (error) return <Padding>error!</Padding>;
 
   const dataSelected = data.incidents.filter(incident =>
     selection.includes(incident.id)
   );
 
-  return (
-    <Box>
-      {selection.length > 0 ? (
-        Object.keys(sources).map(sourceId => {
-          const source = sources[sourceId];
-          const fields = source.fields;
+  return selection.length === 0 ? (
+    <Centered offset={10}>
+      <Hint>Select an incident to view details about it</Hint>
+    </Centered>
+  ) : (
+    Object.keys(sources).map(sourceId => {
+      const source = sources[sourceId];
+      const fields = source.fields;
 
-          return (
-            <>
-              <Padding>
-                <h4>{source.heading}</h4>
-              </Padding>
-              <StructuredListWrapper style={{ margin: 0 }}>
-                <StructuredListBody>
-                  <StructuredListHead>
-                    <StructuredListCell head style={{ width: "30%" }}>
-                      Key
+      return (
+        <>
+          <Padding>
+            <h4>{source.heading}</h4>
+          </Padding>
+          <StructuredListWrapper style={{ margin: 0 }}>
+            <StructuredListBody>
+              <StructuredListHead>
+                <StructuredListCell head style={{ width: "30%" }}>
+                  Key
+                </StructuredListCell>
+                <StructuredListCell head>Value</StructuredListCell>
+                <StructuredListCell head style={{ width: "25%" }}>
+                  Actions
+                </StructuredListCell>
+              </StructuredListHead>
+              {Object.keys(fields).map(fieldId => {
+                const field = fields[fieldId];
+
+                let fieldData =
+                  sourceId === "incident"
+                    ? dataSelected
+                    : dataSelected
+                        .filter(incident => incident.vehicles[0])
+                        .map(incident => incident.vehicles[0]);
+
+                return (
+                  <StructuredListRow key={fieldId}>
+                    <StructuredListCell>{field.label}:</StructuredListCell>
+                    <StructuredListCell>
+                      <b>
+                        {fieldData
+                          .map(record =>
+                            field.view
+                              ? field.view(record[fieldId])
+                              : record[fieldId]
+                          )
+                          .join(", ")}
+                      </b>
                     </StructuredListCell>
-                    <StructuredListCell head>Value</StructuredListCell>
-                    <StructuredListCell head style={{ width: "25%" }}>
-                      Actions
+                    <StructuredListCell>
+                      {isSelectionPresent && false && <Button>p</Button>}{" "}
                     </StructuredListCell>
-                  </StructuredListHead>
-                  {Object.keys(fields).map(fieldId => {
-                    const field = fields[fieldId];
-
-                    let fieldData =
-                      sourceId === "incident"
-                        ? dataSelected
-                        : dataSelected
-                            .filter(incident => incident.vehicles[0])
-                            .map(incident => incident.vehicles[0]);
-
-                    return (
-                      <StructuredListRow key={fieldId}>
-                        <StructuredListCell>{field.label}:</StructuredListCell>
-                        <StructuredListCell>
-                          <b>
-                            {fieldData
-                              .map(record =>
-                                field.view
-                                  ? field.view(record[fieldId])
-                                  : record[fieldId]
-                              )
-                              .join(", ")}
-                          </b>
-                        </StructuredListCell>
-                        <StructuredListCell>
-                          {isSelectionPresent && false && <Button>p</Button>}{" "}
-                        </StructuredListCell>
-                      </StructuredListRow>
-                    );
-                  })}
-                </StructuredListBody>
-              </StructuredListWrapper>
-            </>
-          );
-        })
-      ) : (
-        <Centered offset={10}>
-          <Hint>Select an incident to view details about it</Hint>
-        </Centered>
-      )}
-    </Box>
+                  </StructuredListRow>
+                );
+              })}
+            </StructuredListBody>
+          </StructuredListWrapper>
+        </>
+      );
+    })
   );
-});
+};
--- web/src/components/filters/index.js
+++ web/src/components/filters/index.js
@@ -1,36 +1,31 @@
 import React from "react";
-import Viewport from "../Viewport";
-import connect from "../../lib/connect";
-import styled from "styled-components";
 import Button from "../Button";
 import Flyout from "../flyout";
 import sources from "../../lib/incidentSources";
 import Fields from "./Fields";
 import PaddingRight from "../PaddingRight";
+import { useSelector } from "react-redux";
+import useActions from "../../hooks/useActions";
 
-const Box = styled(Viewport)`
-  padding: 1rem;
-  overflow: visible;
-`;
+export default () => {
+  const filters = useSelector(state => state.filters);
+  const { addFilter, clearFilters } = useActions();
 
-export default connect(state => ({
-  filters: state.filters
-}))(({ filters, actions }) => (
-  <Box>
-    <Fields />
-    <PaddingRight>
-      <Flyout
-        optionSelected={id => actions.addFilter(id)}
-        options={Object.keys(sources.incident.fields).map(id => ({
-          id,
-          text: sources.incident.fields[id].label
-        }))}
-      >
-        Add filter
-      </Flyout>
-    </PaddingRight>
-    {filters.length > 0 && (
-      <Button onClick={actions.clearFilters}>Clear</Button>
-    )}
-  </Box>
-));
+  return (
+    <>
+      <Fields />
+      <PaddingRight>
+        <Flyout
+          optionSelected={addFilter}
+          options={Object.keys(sources.incident.fields).map(id => ({
+            id,
+            text: sources.incident.fields[id].label
+          }))}
+        >
+          Add filter
+        </Flyout>
+      </PaddingRight>
+      {filters.length > 0 && <Button onClick={clearFilters}>Clear</Button>}
+    </>
+  );
+};
