b3b84b2ec72b4808a43a07e9b3f98e21a087db5e
Felix Sebastian <hello@felix-sebastian.com>
2019-10-20 11:17:40 +1100

    make radius input work

--- web/src/components/filters/inputs/Radius.js
+++ web/src/components/filters/inputs/Radius.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-export default props => (
-  <>
-    <input placeholder="center lat" />
-    <input placeholder="center lon" />
-    <input placeholder="distance (kms)" />
-  </>
-);
+export default props => {
+  const filter = useSelector(state => state.filters[props.index]);
+
+  const value = filter.value || {
+    centerLatitude: "",
+    centerLongitude: "",
+    distance: ""
+  };
+
+  const onChange = e =>
+    props.onChange({ ...value, [e.target.dataset.field]: e.target.value });
+
+  return (
+    <>
+      <input
+        type="number"
+        placeholder="center lat"
+        size="5"
+        data-field="centerLatitude"
+        onChange={onChange}
+        value={value.centerLatitude}
+      />
+      <input
+        type="number"
+        placeholder="center lon"
+        size="5"
+        data-field="centerLongitude"
+        onChange={onChange}
+        value={value.centerLongitude}
+      />
+      <input
+        type="number"
+        placeholder="distance (kms)"
+        size="5"
+        data-field="distance"
+        onChange={onChange}
+        value={value.distance}
+      />
+    </>
+  );
+};
