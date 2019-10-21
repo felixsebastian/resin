dc5c0c6abdabb4c5a11e79ef52099907b9061c0b
Luke Mcwha <luke.mcwha@gmail.com>
2019-10-21 21:52:25 +1100

    Added Histogram data

--- web/src/components/Histogram.js
+++ web/src/components/Histogram.js
@@ -11,49 +11,66 @@ import {
   Legend,
   Bar
 } from "recharts";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+import sources from "../lib/incidentSources";
+import { useSelector } from "react-redux";
 
-const data = [
+const INCIDENTS = gql`
+{
+  incidents {
+    id
+    timestamp
+    location {
+      latitude
+      longitude
+    }
+    numVehicles
+    damageSeverity
+    description
+    dca
+    vehicles {
+      vin
+      type
+      registration
+      make
+      model
+      oem
+      yearOfManufacture
+      autonomyLevel
+      sensors {
+        type
+      }
+    }
+  }
+}
+`;
+
+
+// X axis - Weather? (intervals)
+// Y axis - range of applied filters based on the temperature?
+
+
+const graphData = [
   {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
+    name: "Google",
+    incidents: 10,
   },
   {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
+    name: "Delphi",
+    incidents: 1
   },
   {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
+    name: "Nissan",
+    incidents: 3
   },
   {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
+    name:"Tesla",
+    incidents: 13
   },
   {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
+    name:"General Motors",
+    incidents: 10
   }
 ];
 
@@ -61,18 +78,40 @@ const Box = styled(Viewport)`
   width: 100%;
 `;
 
-export default () => (
+export default () => {
+  const { loading, error, data } = useQuery(INCIDENTS);
+
+  if (loading) return (<Box>loading...</Box>);
+  if (error) return (<Box>error!</Box>);
+
+  //console.log(data.incidents[0].vehicles[0].oem);
+/*
+  var oemMap = new Map();
+  for(var incident in data.incidents) {
+    if (oemMap.has(incident.vehicles[0].oem)) {
+      oemMap.set(incident.vehicles[0].oem, oemMap.get(incident.vehicles[0].oem) + 1);
+    }
+    else {
+      oemMap.set(incident.vehicles[0].oem, 1);
+    }
+  }
+  
+ for(var incident in data.incidents) {
+   console.log(incident.vehicles[0].oem);
+ }
+ */
+  return (
   <Box>
     <Centered offset={0}>
-      <BarChart width={600} height={600} data={data}>
+      <BarChart width={600} height={600} data={graphData}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Bar dataKey="pv" fill="#8884d8" />
-        <Bar dataKey="uv" fill="#82ca9d" />
+        <Bar dataKey="incidents" fill="#8884d8" />
       </BarChart>
     </Centered>
   </Box>
-);
+  )
+};
