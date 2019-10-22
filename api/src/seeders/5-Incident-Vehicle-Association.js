f391dc435f7943c66b7652b38b6b1cad87048149
Luke Mcwha <luke.mcwha@gmail.com>
2019-10-22 16:12:58 +1100

    Added data sets

--- api/src/seeders/5-Incident-Vehicle-Association.js
+++ api/src/seeders/5-Incident-Vehicle-Association.js
@@ -153,6 +153,162 @@ module.exports = {
           "createdAt": new Date(),
           "updatedAt": new Date()
         },
+        {
+          "vehicleId": 10,
+          "incidentId": 27,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 10,
+          "incidentId": 28,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 10,
+          "incidentId": 29,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 10,
+          "incidentId": 30,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 10,
+          "incidentId": 31,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 11,
+          "incidentId": 32,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 11,
+          "incidentId": 33,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 10,
+          "incidentId": 34,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 10,
+          "incidentId": 35,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 10,
+          "incidentId": 36,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 10,
+          "incidentId": 37,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 10,
+          "incidentId": 38,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 10,
+          "incidentId": 39,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 10,
+          "incidentId": 40,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 10,
+          "incidentId": 41,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 10,
+          "incidentId": 42,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 10,
+          "incidentId": 43,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 10,
+          "incidentId": 44,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 10,
+          "incidentId": 46,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 5,
+          "incidentId": 47,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 5,
+          "incidentId": 48,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 12,
+          "incidentId": 49,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 12,
+          "incidentId": 50,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 12,
+          "incidentId": 51,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 13,
+          "incidentId": 52,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
+        {
+          "vehicleId": 14,
+          "incidentId": 53,
+          "createdAt": new Date(),
+          "updatedAt": new Date()
+        },
     ]);
 },
 
