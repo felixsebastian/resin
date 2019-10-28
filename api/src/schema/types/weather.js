d98141340e6f4ddb4fc57579a1b74d305c3f7ed3
Cory Mifsud <cory@bento.solutions>
2019-10-28 20:03:37 +1100

    Added weather seeders

---
+++ api/src/schema/types/weather.js
@@ -0,0 +1,19 @@
+export default `
+  type Weather {
+    apparentTemperature
+    cloudCover
+    dewPoint
+    humidity
+    ozone
+    precipIntensity
+    precipProbability
+    pressure
+    summary
+    temperature
+    uvIndex
+    visibility
+    windBearing
+    windGust
+    windSpeed
+  }
+`
