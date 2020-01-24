e6fb90e526c2713102584f626b226d00b170457c
Luke Mcwha <luke@bento.solutions>
2020-01-24 15:26:27 +1100

    luke

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -4,7 +4,8 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(__filename);
-var env = process.env.NODE_ENV || "development";
+// var env = process.env.NODE_ENV || "development";
+var env = "development"
 var config = require(__dirname + "/../config/db.json")[env];
 var db = {};
 if (config.use_env_variable) {
