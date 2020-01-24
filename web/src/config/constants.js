d8134b63ac513a960205117f094d7ab5b61f5269
Luke Mcwha <luke@bento.solutions>
2020-01-24 15:28:50 +1100

    luke

--- web/src/config/constants.js
+++ web/src/config/constants.js
@@ -1,2 +1,2 @@
-export const GRAPHQL = "http://136.186.108.45:4000/graphql";
-export const LOG_IN = "http://136.186.108.45:4000/log-in";
+export const GRAPHQL = "http://54.66.233.153:4000/graphql";
+export const LOG_IN = "http://54.66.233.153:4000/log-in";
